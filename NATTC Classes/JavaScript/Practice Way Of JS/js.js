7eaa5a6564563920cd8d77ab8fb39ba607b7a06d
nabeelmalik122 <nabeelqureshi9095@gmail.com>
2026-08-02 22:56:56 +0500

    Add a Task Using Javascript, Add, Delete

---
+++ NATTC Classes/JavaScript/Practice Way Of JS/js.js
@@ -0,0 +1,69 @@
+// let h1 = document.createElement("h1");
+// h1.innerText = "Hello";
+
+// let p = document.createElement("p");
+// p.innerText = "World";
+
+// document.body.append(h1, p);
+// let h1 = document.createElement("h1");
+// h1.innerText = "Welcome";
+// document.body.appendChild(h1);
+// let h1 = document.createElement("h1");
+// h1.innerText = "Hello";
+
+// document.body.append(h1, " JavaScript");
+
+// show1.value;
+// function show() {
+//   let get = document.getElementById("name").value;
+//   let data = (document.getElementById("result").innerText = get);
+//   console.log(data);
+// }
+// let input = document.getElementById("username");
+// function show() {
+//   console.log((input.value = "Malik Nabeel Khattak"));
+// }
+// let changeName1 = document.getElementById("name");
+// function changeName() {
+//   changeName1.innerHTML = `
+// <h2>Welcome</h2>
+// <p>Hello Nabeel</p>
+// <button>Click Me</button>`;
+// }
+
+// let number = 10.59675;
+// // console.log(Math.random(number));
+// console.log(number.toFixed(0));
+
+// let d = new Date();
+// let currentYear = d.getFullYear();
+// let currentMonth = d.getMonth() + 1;
+// let currentDate = d.getDate();
+// let currentDay = d.getDay();
+// let crrentTime = d.toLocaleTimeString();
+// console.log(`Current Year: ${currentYear}`);
+// console.log(`Current Month: ${currentMonth}`);
+// console.log(`Current Date: ${currentDate}`);
+// console.log(`Current Day: ${currentDay}`);
+// console.log(`Current Time: ${crrentTime}`);
+
+// let weeks = [
+//   "Sunday",
+//   "Monday",
+//   "Tuesday",
+//   "Wednesday",
+//   "Thursday",
+//   "Friday",
+//   "Saturday",
+// ];
+
+// weeks = weeks[d.getDay()];
+// console.log(weeks);
+
+// function greet(num1, num2) {
+//   let sum = num1 + num2;
+//   cons
+// }
+// greet("Nabeel");
+
+
