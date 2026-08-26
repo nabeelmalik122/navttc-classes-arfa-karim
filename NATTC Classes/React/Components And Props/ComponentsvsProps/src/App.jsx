6870b3f47b0ba40199ac7bbd2e13844e2762ea77
nabeelmalik122 <nabeelqureshi9095@gmail.com>
2026-08-27 02:22:31 +0500

    components and props

---
+++ NATTC Classes/React/Components And Props/ComponentsvsProps/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./component/Header";
+
+function Welcome() {
+  return (
+    <>
+      <div>
+        <Header name="Nabeel" fatherName="Khial Min" />
+      </div>
+
+      <button>Button 1</button>
+      <button>Button 2</button>
+      <button>Button 3</button>
+    </>
+  );
+}
+
+export default Welcome;
---
+++ NATTC Classes/React/Components And Props/ComponentsvsProps/src/component/Header.jsx
@@ -0,0 +1,12 @@
+function Header({ name, fatherName }) {
+  return (
+    <>
+      <button>Click Me</button>
+      <h1>
+        Hello {name} {fatherName}
+      </h1>
+    </>
+  );
+}
+
+export default Header;
